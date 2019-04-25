7b728307c418f8ce796beda6dcee644decdd5b70
Maksym Solovianov <maxsolovyanov@gmail.com>
2019-04-25 17:12:47 +0300

    Update HelloWorldLibrary.java

--- src/main/java/HelloWorldLibrary.java
+++ src/main/java/HelloWorldLibrary.java
@@ -1,5 +1,5 @@
 public class HelloWorldLibrary {
     public static void say() {
-        System.out.println("Hello world from Library /version 2/");
+        System.out.println("Hello world from Library /version 3 from GitHub/");
     }
 }
