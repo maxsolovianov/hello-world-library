9ff5ecfaf72f51e6dcf78f372c7ff76a59c4aa35
Maksym Solovianov <maxsolovyanov@gmail.com>
2019-04-25 16:22:59 +0300

    ver 2

--- src/main/java/HelloWorldLibrary.java
+++ src/main/java/HelloWorldLibrary.java
@@ -1,5 +1,5 @@
 public class HelloWorldLibrary {
     public static void say() {
-        System.out.println("Hello world from Library");
+        System.out.println("Hello world from Library /version 2/");
     }
 }
