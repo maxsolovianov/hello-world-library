7b4e5a0f358ffcc9b4f907a3b08f6922952f8d1d
Maksym Solovianov <maxsolovyanov@gmail.com>
2019-04-25 18:33:57 +0300

    add some things

--- src/main/java/HelloWorldLibrary.java
+++ src/main/java/HelloWorldLibrary.java
@@ -1,5 +1,5 @@
 public class HelloWorldLibrary {
     public static void say() {
-        System.out.println("Hello world from Library /version 6 from GitHub after add webhook 3/");
+        System.out.println("Hello world from Library /version 666 from GitHub after add webhook 3/78768678");
     }
 }
