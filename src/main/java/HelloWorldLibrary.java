54045127cb75dae45a1f4c5a4a0f9acd241c6eee
Maksym Solovianov <maxsolovyanov@gmail.com>
2019-04-26 11:38:32 +0300

    add string

--- src/main/java/HelloWorldLibrary.java
+++ src/main/java/HelloWorldLibrary.java
@@ -1,5 +1,5 @@
 public class HelloWorldLibrary {
     public static void say() {
-        System.out.println("Hello world from Library /version FINAL from GitHub after add webhook FINAL");
+        System.out.println("Hello world from Library /version FINAL1 from GitHub after add webhook FINAL1");
     }
 }
